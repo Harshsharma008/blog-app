6318dcd4f6d1f75b88b531ae4b4c89e76c8eb952
Harshsharma008 <harshsharma01012002@gmail.com>
2024-09-27 00:13:23 +0530

    dynamic card

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,4 +1,5 @@
 import Blog from "@/components/blog"
+import CardCarousel from "@/components/dynamic_card"
 import Footer from "@/components/footer"
 import Hero from "@/components/hero"
 import Navbar from "@/components/navbar"
@@ -6,13 +7,64 @@ import Statics from "@/components/statics"
 
 
 const page = () => {
+    const cardData = [
+        {
+            image: "https://dummyimage.com/720x400",
+            category: "CATEGORY",
+            title: "The Catalyzer",
+            description: "Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.",
+            views: "1.2K",
+            comments: 6,
+        },
+        {
+            image: "https://dummyimage.com/720x400",
+            category: "CATEGORY",
+            title: "The Innovator",
+            description: "Viral gochujang ramps flannel narwhal, bespoke paleo celiac flexitarian everyday carry.",
+            views: "950",
+            comments: 8,
+        },
+        {
+            image: "https://dummyimage.com/720x400",
+            category: "CATEGORY",
+            title: "The Disruptor",
+            description: "Palo santo vexillologist pitchfork selvage yuccie pickled 90's kogi heirloom tousled.",
+            views: "870",
+            comments: 12,
+        }, {
+            image: "https://dummyimage.com/720x400",
+            category: "CATEGORY",
+            title: "The Catalyzer",
+            description: "Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.",
+            views: "1.2K",
+            comments: 6,
+        },
+        {
+            image: "https://dummyimage.com/720x400",
+            category: "CATEGORY",
+            title: "The Innovator",
+            description: "Viral gochujang ramps flannel narwhal, bespoke paleo celiac flexitarian everyday carry.",
+            views: "950",
+            comments: 8,
+        },
+        {
+            image: "https://dummyimage.com/720x400",
+            category: "CATEGORY",
+            title: "The Disruptor",
+            description: "Palo santo vexillologist pitchfork selvage yuccie pickled 90's kogi heirloom tousled.",
+            views: "870",
+            comments: 12,
+        },
+    ];
+
     return (
         <>
-        <Navbar/>
-        <Hero/>
-        <Statics/>
-        <Blog/>
-        <Footer/>
+            <Navbar />
+            <Hero />
+            <Statics />
+            <Blog />
+            <CardCarousel cardData={cardData} />
+            <Footer />
         </>
     )
 }
